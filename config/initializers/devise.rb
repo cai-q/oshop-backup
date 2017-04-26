948123cbdd9a7e9de814245f61f05d8c337fcced
蔡乾 <cai.q@foxmail.com>
2017-04-26 16:48:44 +0800

    s

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
 Devise.secret_key = "09c548418de177d7a293d76ae069c4da762e6225ec47824da82c6013f2839480afcd10b99f52d9257356c8ae0513afdcc889"
 #
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :open_wechat, ENV['WX_OPEN_APPID'], ENV['WX_OPEN_APPSECRET'], :scope => 'snsapi_login'
+  provider :open_wechat, ENV['WX_OPEN_APPID'], ENV['WX_OPEN_APPSECRET'], {:scope => 'snsapi_login', :provider_ignores_state => true}
 end
 
 SpreeSocial::OAUTH_PROVIDERS << ['微信开放平台', 'open_wechat', 'true']
